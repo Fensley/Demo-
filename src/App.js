e1ab349d3c90d949729aa6a420dc88fe92383550
Fensley <fortilusnehemie@gmail.com>
2024-04-15 16:46:08 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,51 @@
-function App(){
-    return <h1> Hello React -M </h1>
-}
+import { useState } from "react";
 
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+function App() {
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    setStep(step - 1);
+  }
+  function handleNext() {
+    setStep(step + 1);
+  }
+  console.log(step);
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : " "}>1</div>
+
+        <div className={step >= 2 ? "active" : " "}>2 </div>
+
+        <div className={step >= 3 ? "active" : " "}>3</div>
+      </div>
+      <p className="message">
+        step {step}: {messages[step - 1]}
+      </p>
+      ``
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default App;
